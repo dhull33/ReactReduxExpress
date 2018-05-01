0877f0641d5a375b67aa52f3617e091810fdbecb
David Hull <david.hull1@utexas.edu>
2018-05-01 14:53:02 -0500

    created database module

---
+++ Main/public/javascripts/dataBase.js
@@ -0,0 +1,21 @@
+// set pg-promise to use bluebird as its promise library
+
+const Promise = require('bluebird');
+
+const initOptions = {
+    promiseLib: Promise,
+    capSQL: true
+};
+
+const pgp = require('pg-promise')(initOptions);
+
+const config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'reactbackend',
+    user: 'postgres'
+}
+
+const db = pgp(config);
+
+module.exports = db;
