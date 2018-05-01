4e8d613fa0c02cc2fa856e5c78123721380bb1f9
David Hull <david.hull1@utexas.edu>
2018-05-01 12:07:46 -0500

    Added database connection

--- Main/routes/users.js
+++ Main/routes/users.js
@@ -1,17 +1,43 @@
 var express = require('express');
 var router = express.Router();
+const uuid = require('uuid');
+// set pg-promise to use bluebird as its promise library
+const Promise = require('bluebird');
+
+
+const initOptions = {
+    promiseLib: Promise,
+    capSQL: true
+};
+const pgp = require('pg-promise')(initOptions);
+
+const config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'reactbackend',
+    user: 'postgres'
+}
+
+const db = pgp(config);
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     // Comment out this line:
     //res.send('respond with a resource');
     // And insert something like this instead:
-    res.json([{
-        id: 1,
-        username: "samsepi0l"
-    }, {
-        id: 2,
-        username: "D0loresH4ze"
-    }]);
+    res.json([
+        {
+            name: 'David Hull',
+            email: 'john@gmail.com',
+            phone: '770-564-9087',
+            address: '',
+            city: 'Houston',
+            state: 'Texas',
+            zip: '77006',
+            id: uuid.v4(),
+
+        }
+    ]);
 });
 
 module.exports = router;
