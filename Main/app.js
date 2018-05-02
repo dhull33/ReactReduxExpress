4721815bd6a9700be792f57373ee171156e2cf36
David Hull <david.hull1@utexas.edu>
2018-05-01 21:09:09 -0500

    created database module

--- Main/app.js
+++ Main/app.js
@@ -6,8 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
+
+const db = require('./routes/dataBase');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
